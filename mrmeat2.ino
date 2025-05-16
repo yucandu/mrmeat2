d34b6fb5ce5ff914c9c238b09fc35474b224b242
yucandu <moeburn@gmail.com>
2025-05-16 17:43:34 -0400

    add battery eta estimation

--- mrmeat2.ino
+++ mrmeat2.ino
@@ -17,7 +17,7 @@
 #include "Fonts/Roboto_Condensed_32.h"
 #include "SPIFFS.h"
 #include <Arduino_JSON.h>
-#include "Adafruit_MAX1704X.h"
+//#include "Adafruit_MAX1704X.h"
 #include <BlynkSimpleEsp32.h>
 #include <FastLED.h>
 #include <CircularBuffer.h>
@@ -34,7 +34,7 @@ struct Reading {
 
 CircularBuffer<Reading, HISTORY_SIZE> history;
 
-Adafruit_MAX17048 maxlipo;
+//Adafruit_MAX17048 maxlipo;
 
 #define MYFONT32 &Roboto_Condensed_32
 
@@ -49,11 +49,35 @@ char auth[] = "DU_j5IxaBQ3Dp-joTLtsB0DM70UZaEDd";
 #define is2connectedthreshold 26300  //raw threshold value measured to decide if probe #2 is connected
 #define ETA_INTERVAL 15 //update ETA every 15 seconds
 #define LED_PIN 27
+#define PWRLED_PIN 19
 #define NUM_LEDS 1
 CRGB leds[NUM_LEDS];
 bool speakeron = false;
 
 
+float estimateBatteryTime(float voltage) {
+  // Voltage and corresponding remaining time (in minutes)
+  const int numPoints = 13;
+  float voltages[numPoints] = {4.20, 4.10, 4.00, 3.95, 3.90, 3.85, 3.80, 3.75, 3.70, 3.60, 3.40, 3.30, 3.20};
+  float times[numPoints] =    {1080, 1040, 1005,  920,  835,  760,  694,  635,  576,  420,  150,   60,   15};
+
+
+
+  if (voltage >= voltages[0]) return times[0];
+  if (voltage <= voltages[numPoints - 1]) return times[numPoints - 1];
+
+  for (int i = 0; i < numPoints - 1; i++) {
+    if (voltage <= voltages[i] && voltage > voltages[i + 1]) {
+      float v1 = voltages[i];
+      float v2 = voltages[i + 1];
+      float t1 = times[i];
+      float t2 = times[i + 1];
+      return t1 + (voltage - v1) * (t2 - t1) / (v2 - v1);  // Linear interpolation
+    }
+  }
+
+  return 0; // fallback
+}
 
 
 int8_t PROGMEM TwinkleTwinkle[] = {
@@ -391,28 +415,42 @@ void drawTemps() { //main screen
 
 
   img.drawFastHLine(0,226,240,cmap[setFGC]); //draw the bottom horizontal line
-  img.setCursor(1,231);
-  img.print(WiFi.localIP()); //draw the IP address
-  img.setTextDatum(BR_DATUM); //draw text from the bottom right
 
-
-  
-  //### Battery icon ###
-  if (setIcons) { //draw the icons if that setting is on
+  if (setIcons == 0) {
+    // Text info bar: RSSI and battery voltage
+    img.setCursor(1,231);
+    img.print(WiFi.localIP());
+    String v2String = String(rssi) + "dB/" + String(volts2,2) + "v";
+    img.setTextDatum(BR_DATUM);
+    img.drawString(v2String, 239,239);
+  } else if (setIcons == 1) {
+    // Graphical battery and WiFi icons, show IP address
+    img.setCursor(1,231);
+    img.print(WiFi.localIP());
+    img.setTextDatum(BR_DATUM);
     img.drawRect(214,230,20,9,cmap[setFGC]);
-    img.fillRect(214,230,barx,9,cmap[setFGC]); //draw the battery strength with 'barx' width
+    img.fillRect(214,230,barx,9,cmap[setFGC]);
+    img.drawFastVLine(234,232,4,cmap[setFGC]);
+    img.drawFastVLine(235,232,4,cmap[setFGC]);
+    if (WiFi.status() == WL_CONNECTED) {drawWiFiSignalStrength(200,237,9);}
+  } else if (setIcons == 2) {
+    // Battery time left, graphical battery and WiFi icons
+    float minsLeft = estimateBatteryTime(volts2);
+    int hours = minsLeft / 60;
+    int mins = (int)minsLeft % 60;
+    img.setCursor(1,231);
+    img.printf("Batt left: %dh:%dmin", hours, mins);
+    img.setTextDatum(BR_DATUM);
+    img.drawRect(214,230,20,9,cmap[setFGC]);
+    img.fillRect(214,230,barx,9,cmap[setFGC]);
     img.drawFastVLine(234,232,4,cmap[setFGC]);
     img.drawFastVLine(235,232,4,cmap[setFGC]);
     if (WiFi.status() == WL_CONNECTED) {drawWiFiSignalStrength(200,237,9);}
   }
-  else { //draw the raw wifi/bat info if that setting is on
-    String v2String = String(rssi) + "dB/" + String(volts2,2) + "v";
-    img.drawString(v2String, 239,239);
-  }
 
   img.fillRect(animpos, 232, 4, 4, cmap[setFGC]); //draw our little status indicator
-  animpos += 2; //make it fly
-  if (animpos > 140) {animpos = 100;} //make it wrap around
+  animpos += 1; //make it fly
+  if (animpos > 160) {animpos = 120;} //make it wrap around
 
   img.pushSprite(0, 0);  //and DRAW THAT MOTHERFUCKER
 }
@@ -554,7 +592,7 @@ void drawSettings() {  //if we're in settings mode
 
 
   img.setTextColor(TFT_WHITE);
-  if (setIcons > 1) {setIcons = 0;}  //don't let settings exceed their maximums
+  if (setIcons > 2) {setIcons = 0;}  //don't let settings exceed their maximums
   if (setAlarm > 4) {setAlarm = 0;}
   if (setLEDmode > 3) {setLEDmode = 0;}
   if (setUnits > 2) {setUnits = 0; if (kincreased) {settemp-=273; forceADC(); kincreased = false;}}  //change the alarm set temp too when we switch to kelvin so we don't set off the alarm
@@ -567,7 +605,9 @@ void drawSettings() {  //if we're in settings mode
   String setUnitString;
   String setIconString;
   if (setUnits == 0) {setUnitString = "C";} else if (setUnits == 1) {setUnitString = "F";} else {setUnitString = "K"; if (!kincreased) {settemp+=273; forceADC(); kincreased = true;}}
-  if (setIcons == 0) {setIconString = "N";} else if (setIcons == 1) {setIconString = "Y";} 
+  if (setIcons == 0) {setIconString = "T";} // T for Text
+  else if (setIcons == 1) {setIconString = "Y";} // Y for Yes (icons)
+  else if (setIcons == 2) {setIconString = "B";} // B for Battery left
   img.drawString(setUnitString, 240, 24);
   img.drawNumber(setBGC, 240, 24+24);
   img.drawNumber(setFGC, 240, 24+24+24);
@@ -625,9 +665,9 @@ void forceADC() {  //force an ADC measurement and block the CPU to do it
   adc0 = ads.readADC_SingleEnded(0);
   adc1 = ads.readADC_SingleEnded(1);
   adc2 = ads.readADC_SingleEnded(2);
-  //volts2 = ads.computeVolts(adc2) * 2.0;  //battery voltage measurement
-  volts2 = maxlipo.cellVoltage();
-  adc0 = ads.getLastConversionResults();
+  volts2 = ads.computeVolts(adc2) * 2.0;  //battery voltage measurement
+  //volts2 = maxlipo.cellVoltage();
+  //adc0 = ads.getLastConversionResults();
   if (setUnits == 0) {tempA0f = thermistor.resistanceToTemperature(ADSToOhms(adc0)) - 273.15; tempA1f = thermistor.resistanceToTemperature(ADSToOhms(adc1)) - 273.15;}
   else if (setUnits == 1) {
     tempA0 = thermistor.resistanceToTemperature(ADSToOhms(adc0)) - 273.15;
@@ -636,7 +676,7 @@ void forceADC() {  //force an ADC measurement and block the CPU to do it
     tempA1f = (tempA1 * 1.8) + 32;
   }
   else if (setUnits == 2) {tempA0f = thermistor.resistanceToTemperature(ADSToOhms(adc0)); tempA1f = thermistor.resistanceToTemperature(ADSToOhms(adc1));}
-  barx = mapf (volts2, 3.6, 4.1, 0, 20);
+  barx = mapf (volts2, 3.2, 4.0, 0, 20);
 }
 
 void savePrefs() { //save settings routine
@@ -679,6 +719,7 @@ if (setVolume > 0) {digitalWrite(MUTE_PIN, HIGH);}
 }
 
 void setup() {  //do this on bootup
+setCpuFrequencyMhz(80);
   Serial.begin(115200);
   Serial.println("Hello!");
   initializeCmap();  //do the color map thing
@@ -698,12 +739,12 @@ void setup() {  //do this on bootup
     tft.println("Hello.");
     tft.println("");
   img.setColorDepth(8);  //WE DONT HAVE ENOUGH RAM LEFT FOR 16 BIT AND JOHNNY CASH, MUST USE 8 BIT COLOUR!
-  img.createSprite(239, 239);
+  img.createSprite(240, 240);
   img.fillSprite(TFT_BLUE);
   
   DacAudio.StopAllSounds();
   
-  if (!maxlipo.begin()) {
+  /*if (!maxlipo.begin()) {
     Serial.println(F("Couldnt find Adafruit MAX17048?\nMake sure a battery is plugged in!"));
     delay(2000);
   }
@@ -711,16 +752,18 @@ void setup() {  //do this on bootup
   Serial.print(F("Found MAX17048"));
   Serial.print(F(" with Chip ID: 0x")); 
   Serial.println(maxlipo.getChipID(), HEX);
-  maxlipo.setResetVoltage(2.5);
+  maxlipo.setResetVoltage(2.5);*/
 
   ads.begin();  //fire up the ADC
   ads.setGain(GAIN_ONE);  //1x gain setting is perfect
   ads.startADCReading(ADS1X15_REG_CONFIG_MUX_SINGLE_0, false);
   pinMode(MUTE_PIN, OUTPUT);
+  pinMode(PWRLED_PIN, OUTPUT);
+  digitalWrite(PWRLED_PIN, HIGH);  //turn on the power LED
 
   
   digitalWrite(MUTE_PIN, LOW);  //mute the speaker when we're not playing anything so we don't hear the wifi hiss
-  leds[0] = CRGB(255, 0, 0);
+  leds[0] = CRGB(0, 0, 255);
 FastLED.show(); //turn on the red LED
   pinMode(button1, INPUT_PULLUP); //use internal pullup resistors for the switches so we don't have to solder any
   pinMode(button2, INPUT_PULLUP);
@@ -752,7 +795,17 @@ FastLED.show(); //turn on the red LED
         thermistor.calcCoefficients();
   }
   rssi = WiFi.RSSI();
-  forceADC();
+  forceADC();  // Make sure we have valid temperature readings
+  Reading firstReading = {
+      tempA0f,
+      tempA1f,
+      (float)settemp,
+      0,  // Initial ETA of 0 since we don't have oldtemp yet
+      millis()
+  };
+  history.push(firstReading);
+  oldtemp = tempA0f;  // Initialize oldtemp for future ETA calculations
+  oldtemp2 = tempA1f;
   drawTemps();  //draw one bogus temperature screen while we're booting up
 
   WiFi.mode(WIFI_STA);  //precharge the wifi
@@ -791,25 +844,25 @@ FastLED.show(); //turn on the red LED
         tft.setCursor(0, 0);
         tft.println("Connected!");
         tft.println(WiFi.localIP());
-        leds[0] = CRGB(255, 0, 0);
+        leds[0] = CRGB(0, 0, 255);
 FastLED.show(); 
         delay(250);
         leds[0] = CRGB(0, 0, 0);
 FastLED.show();
         delay(250);
-        leds[0] = CRGB(255, 0, 0);
+        leds[0] = CRGB(0, 0, 255);
 FastLED.show();
         delay(250);
         leds[0] = CRGB(0, 0, 0);
 FastLED.show();
         delay(250);
-        leds[0] = CRGB(255, 0, 0);
+        leds[0] = CRGB(0, 0, 255);
 FastLED.show();
         delay(250);
         leds[0] = CRGB(0, 0, 0);
 FastLED.show();
         delay(250);
-        leds[0] = CRGB(255, 0, 0);
+        leds[0] = CRGB(0, 0, 255);
 FastLED.show();
         delay(250);
         leds[0] = CRGB(0, 0, 0);
@@ -823,7 +876,7 @@ FastLED.show();
     WiFi.begin(wm.getWiFiSSID(), wm.getWiFiPass());
 
   }
-  leds[0] = CRGB(255, 0, 0);
+  leds[0] = CRGB(0, 0, 255);
 FastLED.show(); //make sure red power LED is still on
   initSPIFFS();
   sensors.begin();
@@ -856,20 +909,26 @@ FastLED.show(); //make sure red power LED is still on
 
   server.on("/history", HTTP_GET, [](AsyncWebServerRequest *request){
     JSONVar historyArray;
+    const int maxPoints = 120; // Limit to 30 minutes of data (120 points at 15s intervals)
     
-    for(unsigned int i = 0; i < history.size(); i++) {
+    int startIdx = max(0, (int)history.size() - maxPoints);
+    int count = 0;
+    
+    for(unsigned int i = startIdx; i < history.size() && count < maxPoints; i++) {
         JSONVar reading;
         reading["sensor1"] = String(history[i].temp1);
         reading["sensor2"] = String(history[i].temp2);
         reading["sensor3"] = String(history[i].setTemp);
         reading["sensor4"] = String(history[i].eta);
         reading["timestamp"] = history[i].timestamp;
-        historyArray[i] = reading;
+        historyArray[count++] = reading;
     }
     
     String jsonString = JSON.stringify(historyArray);
     request->send(200, "application/json", jsonString);
-});
+    jsonString = String(); // Free the string
+    historyArray = JSONVar(); // Clear the JSONVar
+  });
 
   events.onConnect([](AsyncEventSourceClient *client){
     if(client->lastId()){
@@ -896,17 +955,17 @@ void loop() {  //do this all the time, main CPU loop
     if (calibrationMode) {sensors.requestTemperatures(); tempC = sensors.getTempCByIndex(0);}
   }
   every(10000) {       //every 10 seconds
-    //volts2 = ads.computeVolts(adc2) * 2.0; //update the battery voltage
-    volts2 = maxlipo.cellVoltage();
-    barx = mapf (volts2, 3.5, 4.0, 0, 20); //update the battery icon length
+    volts2 = ads.computeVolts(adc2) * 2.0; //update the battery voltage
+    //volts2 = maxlipo.cellVoltage();
+    barx = mapf (volts2, 3.2, 4.0, 0, 20); //update the battery icon length
     events.send("ping",NULL,millis());  //update the web interface
     events.send(getSensorReadings().c_str(),"new_readings" ,millis());
 
     if (is2connected) {Blynk.virtualWrite(V4, tempA1f);}
     Blynk.virtualWrite(V2, tempA0f);
     Blynk.virtualWrite(V5, volts2);
-    Blynk.virtualWrite(V11, maxlipo.chargeRate());
-    Blynk.virtualWrite(V12, maxlipo.cellPercent());
+    //Blynk.virtualWrite(V11, maxlipo.chargeRate());
+    //Blynk.virtualWrite(V12, maxlipo.cellPercent());
   }
 
   every(5){ //every 5 milliseconds, so we don't waste battery power
@@ -915,9 +974,9 @@ void loop() {  //do this all the time, main CPU loop
     if (!calibrationMode) {  //if it's not calibration town
       if (setLEDmode == 0) {leds[0] = CRGB(0, 0, 0);
         FastLED.show();}  //configure the red power LED based on settings
-      else if (setLEDmode == 1) {leds[0] = CRGB(40, 0, 0);
+      else if (setLEDmode == 1) {leds[0] = CRGB(0, 0, 40);
         FastLED.show();}
-      else if (setLEDmode == 2) {leds[0] = CRGB(255, 0, 0);
+      else if (setLEDmode == 2) {leds[0] = CRGB(0, 0, 255);
         FastLED.show();}
       else if (setLEDmode == 3) {  //the fancy 4th setting
           float led_brightness = mapf(tempA0f, 0, settemp, 0, 255); 
